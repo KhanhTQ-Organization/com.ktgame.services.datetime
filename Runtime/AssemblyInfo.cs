01cc48ede286d626c52550bb7b8eb07785ba3fb4
KhanhTQ-hub <khanhquang0912qh@gmail.com>
2025-03-24 17:14:41 +0700

    fix: release datetime service

---
+++ Runtime/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("com.ktgame.services.datetime")]
+[assembly: AssemblyProduct("com.ktgame.services.datetime")]
---
+++ Runtime/IDateTimeService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using com.ktgame.core;
+
+namespace com.ktgame.services.datetime
+{
+    public interface IDateTimeService : IInitializable, IPausable, IQuitable
+    {
+        DateTime Now { get; }
+
+        DateTime NowUtc { get; }
+    }
+}
